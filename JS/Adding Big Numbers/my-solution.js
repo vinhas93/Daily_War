0bd4d60b7e8bf1d7ae260d31e1e5bcca03f26fbf
Daniel Vinhas <vinhas.daniel@gmail.com>
2023-04-28 13:22:17 -0300

    refactor[addBigNum]: minnor changes

--- JS/Adding Big Numbers/my-solution.js
+++ JS/Adding Big Numbers/my-solution.js
@@ -23,10 +23,10 @@ function add(a, b) {
   return ten ? ten + sumStr : sumStr;
 }
 
-console.log(add("1", "1")); // "2"
-console.log(add("123", "456")); // "579"
-console.log(add("888", "222")); // "1110"
-console.log(add("1372", "69")); // "1441"
-console.log(add("12", "456")); // "468"
-console.log(add("101", "100")); // "201"
-console.log(add("63829983432984289347293874", "90938498237058927340892374089")); // "91002328220491911630239667963"
+console.log(add("1", "1")+': should be 2');
+console.log(add("123", "456")+': should be 579');
+console.log(add("888", "222")+': should be 1110');
+console.log(add("1372", "69")+': should be 1441');
+console.log(add("12", "456")+': should be 468');
+console.log(add("101", "100")+': should be 201'); 
+console.log(add("63829983432984289347293874", "90938498237058927340892374089")+': should be 91002328220491911630239667963');
