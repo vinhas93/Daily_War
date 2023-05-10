446c7cdf138abf316f0e54614b86ddcefd192ecf
Daniel Vinhas <vinhas.daniel@gmail.com>
2023-05-10 12:18:06 -0300

    chore[calculatingFunc]: my solution - incomplete

---
+++ JS/Calculation with functions/my-solution.js
@@ -0,0 +1,15 @@
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
